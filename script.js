a4cad5fe7c793bd089ba7249bf22b778c954a3cb
Filo <filipingback@gmail.com>
2025-09-30 14:00:36 +0200

    ss

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const btn=document.getElementById("btn");
-    const ruta=document.getElementById("ruta");
+document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById("btn");
+    const ruta = document.getElementById("ruta");
+    const video = document.getElementById("myVideo");
 
-    btn.addEventListener('click',function(){
-    ruta.classList.add("synlig");
-    ruta.classList.remove("osynlig");
-    btn.classList.remove("synlig");
-    btn.classList.add("osynlig");
-    
-                                    
+    btn.addEventListener('click', function() {
+        ruta.classList.add("synlig");
+        ruta.classList.remove("osynlig");
+        btn.classList.remove("synlig");
+        btn.classList.add("osynlig");
+
+        // spela upp från början
+        video.currentTime = 0;
+        video.play();
     });
-    ruta.addEventListener('click',function(){
-    ruta.classList.add("osynlig");
-    ruta.classList.remove("synlig");
-    btn.classList.remove("osynlig");
-    btn.classList.add("synlig");
-    
+
+    ruta.addEventListener('click', function() {
+        ruta.classList.add("osynlig");
+        ruta.classList.remove("synlig");
+        btn.classList.remove("osynlig");
+        btn.classList.add("synlig");
+
+        // pausa videon när rutan stängs
+        video.pause();
     });
 });
